fd15a9610887b04d4eadf5e07aef51edbfac3472
kihuni <stephenkihuni55@gmail.com>
2022-07-12 00:47:58 +0300

    added calc, readme and the test file into simple folder

--- simple_test/calc.test.py
+++ simple_test/calc.test.py
@@ -7,5 +7,9 @@ class TestUnitestMethods(unittest.TestCase):
     def test_add(self):
         result = calc.add(10,15)
         self.assertEqual(result, 25)
+    
+    def test_sub(self):
+        self.assertEqual(calc.sub(11,5), 6)
+        
 if __name__ == '__main__':
     unittest.main()
