61cd6c782c5bf1cdc754aa360962120dafde8e2a
kihuni <stephenkihuni55@gmail.com>
2022-07-11 23:26:59 +0300

    modified the test module

--- test.py
+++ test.py
@@ -7,3 +7,5 @@ class TestUnitestMethods(unittest.TestCase):
     def test_add(self):
         result = calc.add(10,15)
         self.assertEqual(result, 25)
+if __name__ == '__main__':
+    unittest.main()
